ccea3d0240b57bac3aa221b5037afb23fafa4069
FrostByte360 <225485059+FrostByte360@users.noreply.github.com>
2026-09-06 14:12:20 +0800

    Update Digial_Output.ino

--- Digial_Output.ino
+++ Digial_Output.ino
@@ -3,26 +3,27 @@ DIGITAL OUTPUT LED
 ==========================================================================
 
 Description:
-  This program allows the blinking of an LED connected to the GPIO32 pin
-  with a 1-second interval.
+  This program allows the execution of different modes
+  (blinking, alternating, and running) for a set of LED
+  lights connected to the GPIO32, GPIO33, GPIO25, GPIO26,
+  GPIO27, GPIO14 pins with a 1-second interval per cycle.
 
 Programmer:
-  Hans Noe D. Baldomer
+  Mark P. Garcia
 
 Date:
-  2 September 2026
+  6 September 2026
 --------------------------------------------------------------------------*/
 
 // GPIOS
-uint8_t const LED[] = {32,33,25,26,27,14};
+uint8_t const LED[] = {32, 33, 25, 26, 27, 14};
 uint8_t const numb_pins = sizeof(LED)/sizeof(LED[0]);
 int count = 0;
-void setup() {
 
+void setup() {
   for(int i=0; i < 6; i++)  {
     pinMode(LED[i], OUTPUT);
   }
-
 }
 
 #include "LED_Modes.h"
@@ -33,24 +34,4 @@ void loop() {
   count = 0;
   run();
   count = 0;
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
